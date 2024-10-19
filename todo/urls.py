2d6f5a3ca77e6f03c8ac977a2a7edcbb9e017b25
Volverstone <tilek37@mail.ru>
2024-10-20 01:44:32 +0600

    geek

--- todo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('tasks/', views.TaskListAPIView.as_view(), name='task_list'),
-    path('movies/<int:id>/', views.TaskDetailAPIView.as_view(), name='task_detail'),
-]
--- todolist/urls.py
+++ todolist/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/task/', include('todo.urls')),
+    path('api/v1/tasks/', views.TaskListAPIView.as_view()),
+    path('api/v1/tasks/<int:id>/', views.TaskDetailAPIView.as_view()),
 ]
